d96dccba0f1012bd2d7285a392f2b3f2e5fcf8fa
eyarz <eyar@datree.io>
2018-11-13 15:25:35 +0200

    Update test.js

--- test.js
+++ test.js
@@ -5,3 +5,9 @@ const denysdovhan = require('denysdovhan')
 const tj = require('tj')
 
 const contributor = require('contributor')
+
+function myFunction() { // Declare a function
+    document.getElementById("demo").innerHTML = "Hello World!";
+}
+ 
+myFunction(); // Call the function
